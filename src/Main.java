f37122f816a665c12078b8d5520d1aadb97ced42
廣岡　郁弥 <hirookafumiya@hirooka.local>
2020-10-04 13:40:33 +0900

    経過段階

--- src/Main.java
+++ src/Main.java
@@ -1,67 +1,115 @@
+//Main.javaの修正ファイル
+/*
+※行数は、プログラム修正の段階で変化します。
+修正前の9ルール適応時の状態(--rough)
+line: 49, setter method found.
+line: 26, else statement found.
+line: 17, indentation level is too much (more than 2 indent level).
+line: 17, method is too long (over 10 lines).
+line: 49, method is too long (over 10 lines).
 
+修正後の9ルール適応時の状態(--rough)
+line: 69, setter method found.
+line: 38, indentation level is too much (more than 2 indent level).
+line: 69, method is too long (over 10 lines).
+*/
+
+import java.util.stream.IntStream;
+import java.util.ArrayList;
 
 public class Main {
-	static MonsterZoo pz = new MonsterZoo();
+    static MonsterZoo pz = new MonsterZoo();
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
+	setMonsterZukan();
+	setEggs();
+	setuserMonster();
+	//1000ミリ秒（1秒）ずつ止まりながらpz.move()を呼び出し続ける
+	//手持ちのボールが無くなったら終了
+	move_main();
+	final_result();
+    }
 
-		setMonsterZukan();
+    public static void final_result(){
+	System.out.println("ボールがなくなった！");
+	IntStream.range(0,pz.getUserMonster().size())
+	    .filter(value -> pz.getUserMonster().get(value)!="")
+	    .forEach(value -> System.out.println(pz.getUserMonster().get(value)+"を捕まえた．"));
+    }
 
-		//1000ミリ秒（1秒）ずつ止まりながらpz.move()を呼び出し続ける
-		//手持ちのボールが無くなったら終了
-		while(true){
-			try {
-				Thread.sleep(1000);
-				if(pz.getBalls()>0){
-					pz.move();
-					System.out.println("手持ちのボールは"+pz.getBalls()+"個，フルーツは"+pz.getFruits()+"個");
-					System.out.println(pz.getDistance()+"km歩いた．");
-				}else{
-					break;
-				}
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-		}
-
-		System.out.println("ボールがなくなった！");
-
-		for(int i=0;i<pz.getUserMonster().length;i++){
-			if(pz.getUserMonster()[i]!=null){
-				System.out.println(pz.getUserMonster()[i]+"を捕まえた．");
-			}
-		}
+    //モンスターがgetできるかをチャレンジするメソッド
+    public static void move_main(){
+	while(true){
+	    try {
+		Thread.sleep(1000);
+		if(move_distance()){
+		    break;
+		};
+	    }catch(InterruptedException e){
+		e.printStackTrace();
+	    }
 	}
-
-	//テスト用のモンスターデータを登録するメソッド
-	public static void setMonsterZukan(){
-		String tempMonster[] = new String[22];
-		double tempMonsterRare[] = new double[22];
-		tempMonster[0]="イガキン";	tempMonsterRare[0]=9;
-		tempMonster[1]="ナマチュウ";	tempMonsterRare[1]=3;
-		tempMonster[2]="イノウエン";	tempMonsterRare[2]=1;
-		tempMonster[3]="リョージィ";	tempMonsterRare[3]=2;
-		tempMonster[4]="アキモトン";	tempMonsterRare[4]=5;
-		tempMonster[5]="ゴージマ";	tempMonsterRare[5]=4;
-		tempMonster[6]="チュウデン";	tempMonsterRare[6]=6;
-		tempMonster[7]="ヅカホン";	tempMonsterRare[7]=8;
-		tempMonster[8]="ニシムラー";	tempMonsterRare[8]=7;
-		tempMonster[9]="サコーデン";	tempMonsterRare[9]=2;
-		tempMonster[10]="ウッチー";	tempMonsterRare[10]=5;
-		tempMonster[11]="ハヤッシー";	tempMonsterRare[11]=4;
-		tempMonster[12]="キーチー";	tempMonsterRare[12]=3;
-		tempMonster[13]="リョクン";	tempMonsterRare[13]=1;
-		tempMonster[14]="デコポン";	tempMonsterRare[14]=6;
-		tempMonster[15]="カミサン";	tempMonsterRare[15]=5;
-		tempMonster[16]="シスイ";	tempMonsterRare[16]=1;
-		tempMonster[17]="ジョナ";	tempMonsterRare[17]=7;
-		tempMonster[18]="ギダギダ";	tempMonsterRare[18]=2;
-		tempMonster[19]="ミッツー";	tempMonsterRare[19]=8;
-		tempMonster[20]="ゾエサン";	tempMonsterRare[20]=5;
-		tempMonster[21]="キタバー";	tempMonsterRare[21]=3;
-
-		pz.setMonsterZukan(tempMonster);
-		pz.setMonsterRare(tempMonsterRare);
+    }
+    public static boolean move_distance(){
+	if (move_challeng()){//移動が可能な限りチャレンジ
+	    return false;//ループから抜けない
 	}
+	return true;
+    }
+    public static boolean move_challeng(){
+	if(pz.getBalls()>0){
+	    pz.move();
+	    System.out.println("手持ちのボールは"+pz.getBalls()+"個，フルーツは"+pz.getFruits()+"個");
+	    System.out.println(pz.getDistance()+"km歩いた．");
+	    return true;
+	}
+	return false;
+    }
+    //
+    public static void setuserMonster(){
+	ArrayList<String> usermonster = new ArrayList<>();
+	IntStream.range(0,100)
+	    .forEach(value -> usermonster.add(""));
+	pz.setuserMonster(usermonster);
+    }
+    public static void setEggs(){
+	ArrayList<Double> eggDistance = new ArrayList<>();
+	ArrayList<Boolean> egg = new ArrayList<>();
+	IntStream.range(0,10)
+	    .forEach(value -> eggDistance.add(0.0));
+	IntStream.range(0,10)
+	    .forEach(value -> egg.add(false));
+	pz.setEggDistance(eggDistance);
+	pz.setEgg(egg);
+    }
+    //テスト用のモンスターデータを登録するメソッド
+    public static void setMonsterZukan(){
+	ArrayList<String> tempMonster = new ArrayList<>();
+	ArrayList<Double> tempMonsterRare = new ArrayList<>();
+	tempMonster.add("イガキン");	tempMonsterRare.add(9.0);
+	tempMonster.add("ナマチュウ");	tempMonsterRare.add(3.0);
+	tempMonster.add("イノウエン");	tempMonsterRare.add(1.0);
+	tempMonster.add("リョージィ");	tempMonsterRare.add(2.0);
+	tempMonster.add("アキモトン");	tempMonsterRare.add(5.0);
+	tempMonster.add("ゴージマ");	tempMonsterRare.add(4.0);
+	tempMonster.add("チュウデン");	tempMonsterRare.add(6.0);
+	tempMonster.add("ヅカホン");	tempMonsterRare.add(8.0);
+	tempMonster.add("ニシムラー");	tempMonsterRare.add(7.0);
+	tempMonster.add("サコーデン");	tempMonsterRare.add(2.0);
+	tempMonster.add("ウッチー");	tempMonsterRare.add(5.0);
+	tempMonster.add("ハヤッシー");	tempMonsterRare.add(4.0);
+	tempMonster.add("キーチー");	tempMonsterRare.add(3.0);
+	tempMonster.add("リョクン");	tempMonsterRare.add(1.0);
+	tempMonster.add("デコポン");	tempMonsterRare.add(6.0);
+	tempMonster.add("カミサン");	tempMonsterRare.add(5.0);
+	tempMonster.add("シスイ");	tempMonsterRare.add(1.0);
+	tempMonster.add("ジョナ");	tempMonsterRare.add(7.0);
+	tempMonster.add("ギダギダ");	tempMonsterRare.add(2.0);
+	tempMonster.add("ミッツー");	tempMonsterRare.add(8.0);
+	tempMonster.add("ゾエサン");	tempMonsterRare.add(5.0);
+	tempMonster.add("キタバー");	tempMonsterRare.add(3.0);
+	pz.setMonsterZukan(tempMonster);
+	pz.setMonsterRare(tempMonsterRare);
+    }
 
 }
